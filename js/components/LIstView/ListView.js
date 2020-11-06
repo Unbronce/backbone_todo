59ee3d75d9453add9b7ed1e973868eea1638544e
unknown <onlyoneheretic@gmail.com>
2020-11-06 16:50:57 +0200

    add remove method

--- js/components/LIstView/ListView.js
+++ js/components/LIstView/ListView.js
@@ -3,13 +3,23 @@ import Backbone from "backbone";
 import _ from "underscore";
 
 const ListView = Backbone.View.extend({
+  events: {
+    "click #delToDo": "_deletTodo",
+  },
+
   template: _.template($("#list").html()),
 
-  initialize: function () {},
+  initialize: function () {
+    this.listenTo(this.model, "destroy", this.remove);
+  },
 
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },
+
+  _deletTodo: function () {
+    this.model.trigger("destroy", this.model);
   },
 });
 
